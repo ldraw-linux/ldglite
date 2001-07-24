2265e825e2d9e289bb5bb750e4ae4625a39fbba5
dheyse <dheyse>
2001-07-24 12:48:33 +0000

    Changed version number to 0.9.1d

--- main.c
+++ main.c
@@ -6198,7 +6198,7 @@ main(int argc, char **argv)
 
   helpmenunum = glutCreateMenu(menu);
   glutAddMenuEntry(progname             , '\0');
-  glutAddMenuEntry("Version 0.9.1c     ", '\0');
+  glutAddMenuEntry("Version 0.9.1d     ", '\0');
 
   mainmenunum = glutCreateMenu(menu);
   glutAddSubMenu(  "File               ", filemenunum);
