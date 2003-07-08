904a02b9b0c8fcfbb5ca392e51731bbcc522ff9e
Don Heyse <dheyse@hotmail.com>
2003-07-08 15:10:47 +0000

    Plugin shared library stubs.

---
+++ plugstub.c
@@ -0,0 +1,19 @@
+
+// UNIX stubs for now.
+
+char *plugin(plugstruct *plug,
+	   unsigned char *CompleteText,
+	   unsigned char *SelText,
+	   unsigned long *SelStart,
+	   unsigned long *SelLength,
+	   unsigned long *CursoRow,
+	   unsigned long *CursorColum)
+{
+  return NULL;
+}
+
+plugstruct *pluginfo(char *dllname)
+{
+  return NULL;
+}
+
