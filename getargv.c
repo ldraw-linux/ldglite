6b2d10d3f36d73318ee1df46d826f14e7a5dddae
dheyse <dheyse>
2003-08-26 16:54:37 +0000

    Added ifndef HAVE_STRDUP from the python version of macgetargv.c

--- getargv.c
+++ getargv.c
@@ -59,17 +59,19 @@ static char app_name[256];
 static int app_name_inited;
 
 
-/* Duplicate a string to the heap. */
-
-static char *
-strdup(char *src)
+/* Duplicate a string to the heap. We also export this since it isn't standard
+** and others use it
+*/
+#ifndef HAVE_STRDUP
+char *
+strdup(const char *src)
 {
 	char *dst = malloc(strlen(src) + 1);
 	if (dst)
 		strcpy(dst, src);
 	return dst;
 }
-
+#endif
 
 /* Given an FSSpec, return the FSSpec of the parent folder */
 
